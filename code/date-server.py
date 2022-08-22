66d60a2730d982f97f307a3186a9660ff9e23eb5
Israel David <israelcaetano21@hotmail.com>
2022-08-22 19:35:56 -0300

    Atualiza date-server para realizar bind e recepção

--- code/date-server.py
+++ code/date-server.py
@@ -4,6 +4,7 @@ HOST = '127.0.0.1'  # Interface local (localhost)
 PORT = 1234  # Porta para troca de mensagem entre processos
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as skt:
-    skt.bind((HOST, PORT))
-    skt.listen()
-    exit
+    skt.bind((HOST, PORT))  # Aloca um buffer para ouvir conexões
+    msg, end_client = skt.recvfrom(1024)
+
+    print(msg, end_client)
