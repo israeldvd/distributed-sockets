52d0f5c5d0d57dd3303870c1073f3305ffcee907
Israel David <israelcaetano21@hotmail.com>
2022-08-22 20:18:11 -0300

    Adiciona envio a client.py e corrige uso de with/as

--- code/client.py
+++ code/client.py
@@ -3,5 +3,9 @@ import socket
 HOST = '127.0.0.1'  # Interface local (localhost)
 PORT = 1234  # Porta para troca de mensagem entre processos
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM):
-  pass # Usa o objeto socket por meio do gerenciador de contexto -- desfaz necessidade de close()
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as skt:
+    # Usa o objeto socket por meio do gerenciador de contexto
+    print("Socket estabelecido")
+
+    # Envia mensagem simples
+    skt.sendto(input("Digite qualquer coisa:\n->"), (HOST, PORT))
