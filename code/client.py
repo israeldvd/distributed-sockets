1eac83511528b0afba5e64f6ebbf093c1eaa1cdb
Israel David <israelcaetano21@hotmail.com>
2022-08-22 18:57:05 -0300

    Comenta sobre a necessidade de socket.close()

--- code/client.py
+++ code/client.py
@@ -4,4 +4,4 @@ HOST = '127.0.0.1'  # Interface local (localhost)
 PORT = 1234  # Porta para troca de mensagem entre processos
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM):
-  pass
+  pass # Usa o objeto socket por meio do gerenciador de contexto -- desfaz necessidade de close()
