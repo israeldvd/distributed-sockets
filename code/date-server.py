e5a6c1bb2f02b1037b531de24158bfd042bf9d16
Israel David <israelcaetano21@hotmail.com>
2022-08-22 20:08:19 -0300

    Usa data e hora atuais e as envia (sendall)

--- code/date-server.py
+++ code/date-server.py
@@ -1,10 +1,13 @@
 import socket
+from datetime import datetime
 
 HOST = '127.0.0.1'  # Interface local (localhost)
 PORT = 1234  # Porta para troca de mensagem entre processos
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as skt:
     skt.bind((HOST, PORT))  # Aloca um buffer para ouvir conexões
-    msg, end_client = skt.recvfrom(1024)
 
-    print(msg, end_client)
+
+    # Envia hora e data atual
+    skt.sendall(bytes(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+              'utf8'))
