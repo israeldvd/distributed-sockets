0feb22938c45be9277d5974e754700f4acbe0a93
Israel David <israelcaetano21@hotmail.com>
2022-08-22 20:09:01 -0300

    Prepara retornos do recebimento e exibe-os

--- code/date-server.py
+++ code/date-server.py
@@ -6,7 +6,11 @@ PORT = 1234  # Porta para troca de mensagem entre processos
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as skt:
     skt.bind((HOST, PORT))  # Aloca um buffer para ouvir conexões
+    msg, end_client = skt.recvfrom(1024) # Retorna valores mensagem (bytes) e endereço do cliente
 
+    # Exibe esses retornos
+    print("Mensagem do cliente:\n", msg)
+    print("Endereço do cliente:\n", end_client)
 
     # Envia hora e data atual
     skt.sendall(bytes(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
