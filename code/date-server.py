52f96350bd1d335c1187e8c8634512f1ba04722a
Israel David <israelcaetano21@hotmail.com>
2022-08-22 20:14:52 -0300

    Formata date-server.py e especifica envio a client

--- code/date-server.py
+++ code/date-server.py
@@ -5,13 +5,18 @@ HOST = '127.0.0.1'  # Interface local (localhost)
 PORT = 1234  # Porta para troca de mensagem entre processos
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as skt:
-    skt.bind((HOST, PORT))  # Aloca um buffer para ouvir conexões
-    msg, end_client = skt.recvfrom(1024) # Retorna valores mensagem (bytes) e endereço do cliente
+    print("Socket estabelecido")
+
+    # Aloca um buffer para ouvir conexões
+    skt.bind((HOST, PORT))
+
+    # Retorna valores mensagem (bytes) e endereço do cliente
+    msg, end_client = skt.recvfrom(1024)
 
     # Exibe esses retornos
     print("Mensagem do cliente:\n", msg)
     print("Endereço do cliente:\n", end_client)
 
     # Envia hora e data atual
-    skt.sendall(bytes(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-              'utf8'))
+    skt.sendto(bytes(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                     'utf8'), end_client)
