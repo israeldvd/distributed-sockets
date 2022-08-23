5da21b4bf131f0ed79ad6f92414fcc212c1161cc
Israel David <israelcaetano21@hotmail.com>
2022-08-22 21:15:12 -0300

    Atualiza código servidor
    - Assegura espera com try/except
    - Envia informação de seu propósito após receber requisição

--- code/date-server.py
+++ code/date-server.py
@@ -2,21 +2,35 @@ import socket
 from datetime import datetime
 
 HOST = '127.0.0.1'  # Interface local (localhost)
-PORT = 1234  # Porta para troca de mensagem entre processos
+PORT = 12345  # Porta para troca de mensagem entre processos
+TIMEOUT = 20  # Definição de timeout para espera de conexão
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as skt:
-    print("Socket estabelecido")
-
     # Aloca um buffer para ouvir conexões
-    skt.bind((HOST, PORT))
+    try:
+        skt.bind((HOST, PORT))
+    except:
+        print('Erro: falha ao ligar socket ao endereço')
+        exit()
+
+    print("Ligação do socket estabelecida")
+
+    skt.settimeout(TIMEOUT)  # Define um timeout em segundos
 
     # Retorna valores mensagem (bytes) e endereço do cliente
-    msg, end_client = skt.recvfrom(1024)
+    while True:
+        try:
+            msg, end_client = skt.recvfrom(1024)
 
-    # Exibe esses retornos
-    print("Mensagem do cliente:\n", msg)
-    print("Endereço do cliente:\n", end_client)
+            # Exibe esses retornos
+            print("Mensagem do cliente:\n", msg)
+            print("Endereço do cliente:\n", end_client, end="\n\n")
 
-    # Envia hora e data atual
-    skt.sendto(bytes(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-                     'utf8'), end_client)
+            # Envia hora e data atual
+            skt.sendto(bytes(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                             'utf8'), end_client)
+
+        except socket.timeout as e:
+            # Abre exceção após tempo de esperada findado
+            print(e, f': nenhuma conexão após {TIMEOUT} segundos...')
+            break
