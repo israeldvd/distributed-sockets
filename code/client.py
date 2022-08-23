f91eea7b79113376fe2df0e3232b1c53b4eb210e
Israel David <israelcaetano21@hotmail.com>
2022-08-22 21:11:13 -0300

    Atualiza o código client-side para receber dados
    - Atualiza porta
    - Define timeout
    - Abre tratamento try/exception

--- code/client.py
+++ code/client.py
@@ -1,11 +1,24 @@
 import socket
 
 HOST = '127.0.0.1'  # Interface local (localhost)
-PORT = 1234  # Porta para troca de mensagem entre processos
+PORT = 12345  # Porta para troca de mensagem entre processos
+TIMEOUT = 10  # Espera alguns segundos antes de desistir
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as skt:
     # Usa o objeto socket por meio do gerenciador de contexto
     print("Socket estabelecido")
 
     # Envia mensagem simples
-    skt.sendto(input("Digite qualquer coisa:\n->"), (HOST, PORT))
+    skt.sendto(bytes(input("Digite qualquer coisa:\n->"), 'utf8'),
+               (HOST, PORT))
+
+    try:
+        msg = skt.recv(1024)
+
+        # Exibe esses retornos
+        print("Mensagem do servidor:\n", msg.decode('utf8'), end="\n\n")
+
+    except socket.timeout as e:
+        print(e, f': nenhuma conexão após {TIMEOUT} segundos...')
+
+    print("Finalização do socket")
